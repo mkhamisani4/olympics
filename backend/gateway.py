392a93d674218b6ad3f7cb20752510cf60f4534b
Mohammed Khamisani <mohammedkhamisani@mohammeds-mbp.lan>
2025-04-20 01:08:46 -0700

    our first sample query

--- backend/gateway.py
+++ backend/gateway.py
@@ -12,14 +12,16 @@ def get_db_connection():
     )
     return conn
 
-# a sample query for now
+# a sample query for now - same one from our phase 2 doc
 @app.route('/api/data', methods=['GET'])
 def get_data():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM olympic_medal_tally_history WHERE total > 100 ORDER BY total ASC LIMIT 1;')
+    cursor.execute('SELECT medalTally.country_noc, countryProfile.country, SUM(medalTally.gold)   AS total_gold, SUM(medalTally.silver) AS total_silver, SUM(medalTally.bronze) AS total_bronze, SUM(medalTally.total)  AS total_medals FROM Olympic_Medal_Tally_History AS medalTally JOIN Olympic_Country_Profiles AS countryProfile ON medalTally.country_noc = countryProfile.noc JOIN Olympic_Games_Summary AS gameSummary ON medalTally.edition_id = gameSummary.edition_id WHERE gameSummary.year::INTEGER >= 2004 GROUP BY medalTally.country_noc, countryProfile.country ORDER BY total_medals DESC;')
+    columns = ["country_noc", "country", "goldMedals", "silverMedals", "bronzeMedals", "totalMedals"]
     search_results = cursor.fetchall()
-    return jsonify(search_results)
+    results = [dict(zip(columns, result)) for result in search_results]
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
