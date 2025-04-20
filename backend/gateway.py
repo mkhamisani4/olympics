466dfb9dd7585c5714811f1732266e143819d569
Mohammed Khamisani <mohammedkhamisani@mohammeds-mbp.lan>
2025-04-19 21:40:37 -0700

    initial stuff

---
+++ backend/gateway.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import psycopg2
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname="", user="", password="", host="",
+        port=""
+    )
+    return conn
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    print("My name is Mo!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
